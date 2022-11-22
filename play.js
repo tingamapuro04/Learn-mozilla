363be69da72995fe048b3c0c922efeff32be8d9e
tingamapuro04 <alponcemobutu100@gmail.com>
2022-11-22 21:42:54 +0300

    Add names

--- play.js
+++ play.js
@@ -1,7 +1,21 @@
 const sec = document.querySelector('section');
+const para1 = document.createElement('p')
 
 const requestUrl = 'https://mdn.github.io/learning-area/javascript/oojs/tasks/json/sample.json';
+
+function displayInfo(data) {
+  // console.log(data)
+  const kittenObj = JSON.parse(data);
+  const names = []
+  kittenObj.forEach((element) => {
+    names.push(element.name)
+  })
+  const ship = names.join(' - ')
+  para1.textContent = ship
+}
 
 fetch(requestUrl)
   .then((response) => response.text())
   .then((text) => displayInfo(text));
+
+sec.appendChild(para1);
