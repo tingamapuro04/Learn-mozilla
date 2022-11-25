14b4900c98268e9a3447a84f36ca35ccd4599278
tingamapuro04 <alponcemobutu100@gmail.com>
2022-11-25 21:33:28 +0300

    send xhttps request

--- play.js
+++ play.js
@@ -4,6 +4,13 @@ const mimi = {
   ethnicity: 'Luo',
 };
 
-const jj = JSON.stringify(mimi);
-
-console.log(jj);
+const xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    // Typical action to be performed when the document is ready:
+    // document.getElementById('demo').innerHTML = xhttp.responseText;
+    console.log(xhttp.responseText);
+  }
+};
+xhttp.open('GET', 'mimi.json', true);
+xhttp.send();
