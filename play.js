8c775e824196479d4912e911e34738b7d9c782f3
tingamapuro04 <alponcemobutu100@gmail.com>
2022-11-26 01:54:22 +0300

    Complete promise

--- play.js
+++ play.js
@@ -1,27 +1,41 @@
 // Synchronous Programming
-const http = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
-console.log(http)
+// const http = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+// console.log(http)
 
-// http
-//   .then((response) => response.json())
-//   .then((kula) => {
-//     console.log(kula[0].name);
-//   });
+// // http
+// //   .then((response) => response.json())
+// //   .then((kula) => {
+// //     console.log(kula[0].name);
+// //   });
 
-const req = 'bad-scheme://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json';
+// const req = 'bad-scheme://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json';
 
-fetch(req)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`Hii ndo error: ${response.status}`)
-    }
-    return response.json();
-  })
-  .then((oooh) => {
-    console.log(oooh[0].name);
+// fetch(req)
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error(`Hii ndo error: ${response.status}`)
+//     }
+//     return response.json();
+//   })
+//   .then((oooh) => {
+//     console.log(oooh[0].name);
+//   })
+//   .catch((error) => {
+//     console.log(`We have ${error}<<<<< error`);
+//   })
+
+// console.log('Started request');
+
+
+
+const fetchPromise1 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found');
+const fetchPromise3 = fetch('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json');
+
+Promise.any([fetchPromise2, fetchPromise3, fetchPromise1])
+  .then((responses) => {
+    console.log(`${responses.url}: ${responses.status}`);
   })
   .catch((error) => {
-    console.log(`We have ${error}<<<<< error`);
+    console.error(`Failed to fetch: ${error}`);
   })
-
-console.log('Started request');
