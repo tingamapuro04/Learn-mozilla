32ebec78b7bf62015814846ab8e69c43b3caccc1
tingamapuro04 <alponcemobutu100@gmail.com>
2022-11-25 20:40:17 +0300

    Complete assignment for JSON

--- play.js
+++ play.js
@@ -1,17 +1,40 @@
 const sec = document.querySelector('section');
-const para1 = document.createElement('p')
+const para1 = document.createElement('p');
+const para2 = document.createElement('p');
+const para3 = document.createElement('p');
+const para4 = document.createElement('p');
+let NoOfKittens = 0;
+let female = 0;
+let male = 0;
 
 const requestUrl = 'https://mdn.github.io/learning-area/javascript/oojs/tasks/json/sample.json';
 
 function displayInfo(data) {
-  // console.log(data)
+  console.log(data);
   const kittenObj = JSON.parse(data);
-  const names = []
+  const names = [];
   kittenObj.forEach((element) => {
-    names.push(element.name)
-  })
-  const ship = names.join(' - ')
-  para1.textContent = ship
+    NoOfKittens += element.kittens.length;
+    // if (element.kittens.gender === 'm') {
+    //   male += 1;
+    // } else {
+    //   female += 1;
+    // }
+
+    element.kittens.forEach((kitt) => {
+      if (kitt.gender === 'f') {
+        female += 1;
+      } else if (kitt.gender === 'm') {
+        male += 1;
+      }
+    });
+    names.push(element.name);
+  });
+  const ship = names.join(', ');
+  para1.textContent = ship;
+  para2.textContent = `We have ${NoOfKittens} kittens from all the mothers.`;
+  para3.textContent = `We have ${male} male kittens.`;
+  para4.textContent = `We have ${female} female kittens.`;
 }
 
 fetch(requestUrl)
@@ -19,3 +42,6 @@ fetch(requestUrl)
   .then((text) => displayInfo(text));
 
 sec.appendChild(para1);
+sec.appendChild(para2);
+sec.appendChild(para3);
+sec.appendChild(para4);
